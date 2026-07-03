096e6e161574adde487ec152e933c8630c0deb24
Nishant <142477092+nishant-iith@users.noreply.github.com>
2026-07-03 23:37:17 +0530

    Solve: binary-tree-inorder-traversal (#2 - 03-07-2026)

---
+++ 0094-binary-tree-inorder-traversal/binary-tree-inorder-traversal_03-07-2026_2.java
@@ -0,0 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> inorderTraversal(final TreeNode root) {
+        final var res = new ArrayList<Integer>();
+        var curr = root;
+        
+        while (curr != null) {
+            if (curr.left == null) {
+                res.add(curr.val);
+                curr = curr.right; // move to next right node
+            } else { // has a left subtree
+                var pre = curr.left;
+                while (pre.right != null) { // find rightmost
+                    pre = pre.right;
+                }
+                pre.right = curr; // put cur after the pre node
+                
+                var temp = curr; // store cur node
+                curr = curr.left; // move cur to the top of the new tree
+                temp.left = null; // original cur left be null, avoid infinite loops
+            }
+        }
+        return res;
+    }
+}
